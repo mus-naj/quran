6e913d5a3b2c315fbf7ef0e95d855579cb683bc0
Mustafa <mustafa.alnajjar@viitech.net>
2017-06-21 02:54:05 +0300

    ignore file added, includes function is defined for safari browser

--- js/compatibility.js
+++ js/compatibility.js
@@ -71,6 +71,21 @@ if (!Array.prototype.map) {
     };
 }
 
+if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+        'use strict';
+        if (typeof start !== 'number') {
+            start = 0;
+        }
+
+        if (start + search.length > this.length) {
+            return false;
+        } else {
+            return this.indexOf(search, start) !== -1;
+        }
+    };
+}
+
 if (!Array.prototype.includes) {
     Array.prototype.includes = function(searchElement /*, fromIndex*/) {
         'use strict';
